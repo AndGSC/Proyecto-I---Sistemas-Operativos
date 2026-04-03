9fb1844af4e65dbbb2a7b77e75043bad52c1f9a8
AndGSC <andrey.solis.carvajal@est.una.ac.cr>
2026-04-03 12:07:28 -0600

    Update cola.h

--- terminal_carga/include/cola.h
+++ terminal_carga/include/cola.h
@@ -1,1 +1,27 @@
-    
+#ifndef COLA_H
+#define COLA_H
+
+#include "camion.h"
+
+/* Nodo de la cola */
+typedef struct Nodo {
+    Camion *camion;
+    struct Nodo *sig;
+} Nodo;
+
+/* Estructura de la cola */
+typedef struct {
+    Nodo *frente;
+    Nodo *final;
+    int tamano;
+} Cola;
+
+/* Operaciones de la cola */
+void init_cola(Cola *c);
+int cola_vacia(Cola *c);
+void insertar_camion(Cola *c, Camion *camion);
+Camion *sacar_camion(Cola *c);
+Camion *primer_camion(Cola *c);
+void destruir_cola(Cola *c);
+
+#endif
