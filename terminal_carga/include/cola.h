60e54e2aa316361af3300d87fff62ef7765168cf
AndGSC <andrey.solis.carvajal@est.una.ac.cr>
2026-04-11 18:51:45 -0600

    Refactor comments in cola.h
    Updated comments in cola.h for clarity and consistency.

--- terminal_carga/include/cola.h
+++ terminal_carga/include/cola.h
@@ -9,19 +9,25 @@ typedef struct Nodo {
     struct Nodo *sig;
 } Nodo;
 
-/* Estructura de la cola */
+/* struct de la cola */
 typedef struct {
     Nodo *frente;
     Nodo *final;
     int tamano;
 } Cola;
 
-/* Operaciones de la cola */
+/* 
+Operaciones de la cola
+Nota: esta cola no maneja sincronizacion interna.
+Si varios hilos acceden a ella, debe protegerse externamente con mutex.
+*/
 void init_cola(Cola *c);
 int cola_vacia(Cola *c);
 void insertar_camion(Cola *c, Camion *camion);
 Camion *sacar_camion(Cola *c);
 Camion *primer_camion(Cola *c);
+
+/* Libera solo los nodos de la cola, no los camiones */
 void destruir_cola(Cola *c);
 
 #endif
